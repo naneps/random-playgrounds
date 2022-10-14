0f95ba0cf87fa067fb19c7dfb57e6a880d16454b
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-10-14 09:51:46 +0700

    ⬆️⬆️⬆️Move To View

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template >
-  <div>
-    <TicTacToe/>
+  <div class="container mx-auto">
+    <Navbar />
+    <div v-auto-animate>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 <script>
 import TicTacToe from "./pages/TicTacToe.vue";
+import Navbar from "./components/Navbar.vue";
 export default {
-    components: { TicTacToe }
-}
+  components: { TicTacToe, Navbar },
+};
 </script>
 <style >
-  
 </style>
